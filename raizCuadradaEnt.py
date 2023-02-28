a81af51c16faef362aedce8327f911f631c61e01
eduardost <30332574+eduardost@users.noreply.github.com>
2023-02-28 10:05:34 -0300

    Update raizCuadradaEnt.py

--- raizCuadradaEnt.py
+++ raizCuadradaEnt.py
@@ -5,14 +5,15 @@ def raizCuadradaEnt (numero):
 		i=1
 		while(i*i <= numero): ## complejidad O(log N)
 				i*=2 #voy multiplicando por 2
-		valor= busquedaBinaria(numero,i/2,i) #cuando me pase, llamo a la busqueda.
+		valor= busquedaBinaria(numero,i//2,i) #cuando me pase, llamo a la busqueda.
+        	## uso // pues quiero un resultado entero, sino tendria que castear.
 		return valor
 
 def busquedaBinaria(x,i,f): #le paso el numero, inicio y fin. Complejidad O(log N)
 	if(i==f):
 		return i
 	else:
-		medio=(i+f)/2
+		medio=(i+f)//2
 		if(x==medio*medio):
 			return medio
 		if(x>medio*medio):
@@ -22,7 +23,7 @@ def busquedaBinaria(x,i,f): #le paso el numero, inicio y fin. Complejidad O(log 
 
 
 #main
-#print (raizCuadradaEnt(2))
+print (raizCuadradaEnt(2))
 print (raizCuadradaEnt(9))
 print (raizCuadradaEnt(18))
 print (raizCuadradaEnt(199))
